334f4496badfc4599c2766c82927d9495f3eb8f3
Kai Mast <kaimast@cs.wisc.edu>
2024-05-25 17:49:50 -0700

    Add documentation and NEWS

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,21 @@ impl Condvar {
         Self::default()
     }
 
+    /// Wake up all tasks currently waiting on this condition variable.
     pub fn notify_all(&self) {
         self.inner.notify_waiters();
     }
 
+    /// Wake up exactly one task that is currently waiting on this condition variable
+    /// If no task is waiting, none will be woken up.
     pub fn notify_one(&self) {
         // Not supported yet
         self.inner.notify_waiters();
     }
 
+    /// Wait to be woken up while holding a lock
+    /// This function will automatically release the lock before waiting
+    /// and reacquires it after waking up
     pub async fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
         let fut = self.inner.notified();
         tokio::pin!(fut);
@@ -31,6 +37,7 @@ impl Condvar {
         mutex.lock().await
     }
 
+    /// Same as `Self::wait` but for a parking_lot Mutex
     #[cfg(feature = "parking_lot")]
     pub async fn parking_lot_wait<'a, T>(
         &self,
@@ -47,6 +54,7 @@ impl Condvar {
         mutex.lock()
     }
 
+    /// Same as `Self::wait` but for a parking_lot read-write lock
     #[cfg(feature = "parking_lot")]
     pub async fn parking_lot_rw_wait<'a, T>(
         &self,
